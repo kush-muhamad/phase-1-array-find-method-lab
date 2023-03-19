7fa328ec041126c8cf22fd4c071da358d5d570a9
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-20 01:37:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+function superbowlWin(data) {
+    let result = data.find( result => result.result === "W" )
+    return !!result ? result.year : undefined
+}
